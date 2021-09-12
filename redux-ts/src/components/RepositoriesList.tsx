f4e4932b024d5f1cecff2fbe0a37304b51a036c4
vy2907 <vynguyen29071997@gmail.com>
2021-09-12 23:23:36 +0700

    43

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,25 +1,31 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../state';
+import { useState } from "react";
+import { useTypedSelector } from "../hook/useTypedSelectors";
+import { useActions } from "../hook/useAction";
 
 const RepositroiesList: React.FC = () => {
-    const [term, setTerm] = useState('');
-    const dispatch = useDispatch();
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        dispatch(actionCreators.searchRepositories(term));
-    };
+    searchRepositories(term);
+  };
 
-    return ( 
+  return (
     <div>
-        <form onSubmit={onSubmit}>
-            <input value={term} onChange={e => setTerm(e.target.value)}/>
-            <button>Search</button>
-        </form>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
-    );
+  );
 };
 
 export default RepositroiesList;
--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -1,35 +1,37 @@
-import axios from 'axios';
-import { Dispatch } from 'redux';
-import { ActionType } from '../action-types';
-import { Action } from '../action';
+import axios from "axios";
+import { Dispatch } from "redux";
+import { ActionType } from "../action-types";
+import { Action } from "../action";
 
 export const searchRepositories = (term: string) => {
-    return async (dispatch: Dispatch<Action>) => {
-        dispatch({
-            type: ActionType.SEARCH_REPOSITORIES
-        });
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
 
-        try {
-            const { data } = await axios.get('http://registry.npmjs.org/-/v1/search', {
-                params: {
-                    text: term
-                 }
-            });
-
-            const names = data.objects.map((result: any) => {
-                return result.package.name;
-            });
-
-            dispatch({
-                type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
-                payload: names,
-            }); 
-
-        } catch (err) {
-            dispatch({
-                type: ActionType.SEARCH_REPOSITORIES_ERROR,
-                payload: err.message,
-            })
+    try {
+      const { data } = await axios.get(
+        "http://registry.npmjs.org/-/v1/search",
+        {
+          params: {
+            text: term,
+          },
         }
-    };
-}; 
+      );
+
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (err) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
